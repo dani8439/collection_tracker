f63efdf6a3221140f1bf034dda711e42b0dda0a6
Dani Schuhman <dani8439@aol.com>
2018-09-25 17:14:33 +0000

    Figuring out how to add pattern to collection

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,2 +1,27 @@
 class PatternsController < ApplicationController
+  get '/patterns' do
+    @pattern = Pattern.all
+    erb :'patterns/patterns'
+  end
+
+  get '/patterns/new' do
+    erb :'patterns/new'
+  end
+
+  get '/patterns/:id' do
+    @pattern = Pattern.find_by_id(params[:id])
+    erb :'patterns/show_pattern'
+  end
+
+  post '/patterns' do
+    @pattern = Pattern.create(name: params[:name], theme: params[:theme])
+    @pattern.piece = Piece.find_or_create_by(name: params[:piece_name])
+    
+  end
+
+  get '/patterns/:id/edit' do
+  end
+
+  patch '/patterns/:id' do
+  end
 end
--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,20 +1,4 @@
 class PiecesController < ApplicationController
 
-  get '/patterns' do
-  end
 
-  get '/patterns/new' do
-  end
-
-  get '/patterns/:id' do
-  end
-
-  post '/patterns' do
-  end
-
-  get '/patterns/:id/edit' do
-  end
-
-  patch '/patterns/:id' do
-  end
 end
