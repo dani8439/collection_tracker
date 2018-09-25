8623c170661b3cff374d08e9af03e5c29474c67a
Dani Schuhman <dani8439@aol.com>
2018-09-25 17:27:40 +0000

    patterns controller routes

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -4,8 +4,13 @@ class PatternsController < ApplicationController
     erb :'patterns/patterns'
   end
 
+
   get '/patterns/new' do
-    erb :'patterns/new'
+    if logged_in?
+      erb :'patterns/create_pattern'
+    else
+      redirect :'/login'
+    end
   end
 
   get '/patterns/:id' do
@@ -16,7 +21,10 @@ class PatternsController < ApplicationController
   post '/patterns' do
     @pattern = Pattern.create(name: params[:name], theme: params[:theme])
     @pattern.piece = Piece.find_or_create_by(name: params[:piece_name])
-    
+    @pattern.save
+
+    redirect :"/patterns/#{@pattern.id}"
+
   end
 
   get '/patterns/:id/edit' do
