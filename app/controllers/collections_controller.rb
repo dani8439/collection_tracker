bb94267ea8d0a522d0a82bca6f2de7be4fa92241
Dani Schuhman <dani8439@aol.com>
2018-10-09 19:04:29 +0000

    removed unnecessary code from collections controller

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -10,13 +10,6 @@ class CollectionsController < ApplicationController
     end
   end
 
-  get '/collections/new' do
-    if logged_in?
-      erb :'/collections/create_collection'
-    else
-      redirect :'/login'
-    end
-  end
 
   get '/collections/:id' do
     if logged_in?
@@ -27,15 +20,6 @@ class CollectionsController < ApplicationController
     end
   end
 
-  # Do I need this? Want to edit Pieces a collector has, not collection itself?
-  get '/collections/:id/edit' do
-    if logged_in?
-      @collection = Collection.find_by_id(params[:id])
-      erb :'/collections/edit'
-    else
-      redirect :'/login'
-    end
-  end
 
   post '/collections' do
     if !params[:collection][:user_id].empty?
@@ -48,11 +32,4 @@ class CollectionsController < ApplicationController
     end
   end
 
-  patch '/collections/:id' do
-    # @collection = Collection.find_by_id(params[:id])
-    # if !params[:collection][:]
-  end
-
-  delete '/collections/:id/delete' do
-  end
 end
